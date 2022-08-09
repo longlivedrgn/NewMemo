5d0506c887045a5a34bc62735c07c212a5c531b6
longlivedrgn <kimye9808@naver.com>
2022-08-09 17:12:17 +0900

    class memo

---
+++ NewMemo/Model/Memo.swift
@@ -0,0 +1,22 @@
+//
+//  Memo.swift
+//  NewMemo
+//
+//  Created by 김용재 on 2022/08/09.
+//
+
+import Foundation
+
+class Memo {
+    var content: String
+    var insertDate: Date
+    
+    init(content: String) {
+        self.content = content
+        insertDate = Date()
+    }
+    static var dummyMemoList = [
+    Memo(content: "Lorem Ipsum"),
+    Memo(content: "Subscribe + 👍 = ♥️")
+    ]
+}
