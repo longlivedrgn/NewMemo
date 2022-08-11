b7af25d36df1f48198d7dd0de1c2b4ae714654ef
longlivedrgn <kimye9808@naver.com>
2022-08-11 10:44:01 +0900

    취소 action 추가

--- NewMemo/ComposeViewController.swift
+++ NewMemo/ComposeViewController.swift
@@ -9,21 +9,11 @@ import UIKit
 
 class ComposeViewController: UIViewController {
 
+    @IBAction func close(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
