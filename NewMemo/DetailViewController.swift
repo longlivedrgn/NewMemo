fcec78ce6ce0090dbcbb435e8dcee9088e2f3bc8
longlivedrgn <kimye9808@naver.com>
2022-08-11 14:21:53 +0900

    Detail tableview로 데이터 전달

--- NewMemo/DetailViewController.swift
+++ NewMemo/DetailViewController.swift
@@ -8,11 +8,20 @@
 import UIKit
 
 class DetailViewController: UIViewController {
+    
+    var memo: Memo?
+    
+    let formatter: DateFormatter = {
+       let f = DateFormatter()
+        f.dateStyle = .long
+        f.timeStyle = .short
+        f.locale = Locale(identifier: "Ko_kr")
+        return f
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
 
@@ -37,11 +46,11 @@ extension DetailViewController: UITableViewDataSource {
         switch indexPath.row {
         case 0:
             let cell = tableView.dequeueReusableCell(withIdentifier: "memoCell", for: indexPath)
-            
+            cell.textLabel?.text = memo?.content
             return cell
         case 1:
             let cell = tableView.dequeueReusableCell(withIdentifier: "dateCell", for: indexPath)
-            
+            cell.textLabel?.text = formatter.string(for: memo?.insertDate)
             return cell
         default:
             fatalError()
