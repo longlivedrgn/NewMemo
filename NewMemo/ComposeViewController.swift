cd055511fcd55d109463b867043b5f3692517cd3
longlivedrgn <kimye9808@naver.com>
2022-08-12 17:55:52 +0900

    메모 편집

--- NewMemo/ComposeViewController.swift
+++ NewMemo/ComposeViewController.swift
@@ -8,6 +8,8 @@
 import UIKit
 
 class ComposeViewController: UIViewController {
+    
+    var editTarget: Memo?
 
     @IBAction func close(_ sender: Any) {
         dismiss(animated: true, completion: nil)
@@ -22,9 +24,16 @@ class ComposeViewController: UIViewController {
         
 //        let newMemo = Memo(content: memo)
 //        Memo.dummyMemoList.append(newMemo)
-        DataManager.shared.addNewMemo(memo)
         
-        NotificationCenter.default.post(name: ComposeViewController.newMemoDidInsert, object: nil)
+        if let target = editTarget {
+            target.content = memo
+            DataManager.shared.saveContext()
+            NotificationCenter.default.post(name: ComposeViewController.memoDidChange, object: nil)
+            
+        } else {
+            NotificationCenter.default.post(name: ComposeViewController.newMemoDidInsert, object: nil)
+            DataManager.shared.addNewMemo(memo)
+        }
         
         dismiss(animated: true, completion: nil)
     }
@@ -33,12 +42,19 @@ class ComposeViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-    
-
+        
+        if let memo = editTarget {
+            navigationItem.title = "메모 편집"
+            memoTextView.text = memo.content
+        } else {
+            navigationItem.title = "새 메모"
+            memoTextView.text = ""
+        }
     }
 }
 
 // 노티피케이션
 extension ComposeViewController {
     static let newMemoDidInsert = Notification.Name(rawValue: "newMemoDidInsert")
+    static let memoDidChange = Notification.Name(rawValue: "memoDidChange")
 }
